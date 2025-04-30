70dbabff32d81399e8a66e25b60042da0ad87e43
Gabriel Gaudreault <93156290+gabgaud@users.noreply.github.com>
2025-04-30 13:24:38 -0400

    Update default.nix

--- 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
+++ 2025-02-nix-nvidia-ollama/nix/hosts/nixos/nix-llm/default.nix
@@ -21,12 +21,12 @@
     interfaces.ens18 = {
       useDHCP = false;
       ipv4.addresses = [{
-        address = "10.42.37.100";
+        address = "172.16.16.143";
         prefixLength = 24;
       }];
     };
-    defaultGateway = "10.42.37.254";
-    nameservers = [ "10.42.37.254" ];
+    defaultGateway = "172.16.16.1";
+    nameservers = [ "172.16.16.1" ];
   };
 
   # System localization
